dc0165f1589c22c5223996445c05e3e0880393f7
Ruling <rulingghost@gmail.com>
2014-08-13 20:54:28 +0800

    Update vendorsetup.sh

--- device/oppo/find7/vendorsetup.sh
+++ device/oppo/find7/vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo cm_find7-user
-add_lunch_combo cm_find7-userdebug
-add_lunch_combo cm_find7-eng
+add_lunch_combo mk_find7-user
+add_lunch_combo mk_find7-userdebug
+add_lunch_combo mk_find7-eng
